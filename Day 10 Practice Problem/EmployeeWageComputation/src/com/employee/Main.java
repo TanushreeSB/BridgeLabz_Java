c96b55b9f302e5ee0e773dbfced3403507962ef4
Tanushree Borase <tb2599@srmist.edu.in>
2026-04-21 23:17:33 +0530

    UC2: Added part-time employee wage

--- Day 10 Practice Problem/EmployeeWageComputation/src/com/employee/Main.java
+++ Day 10 Practice Problem/EmployeeWageComputation/src/com/employee/Main.java
@@ -7,4 +7,26 @@ public class Main {
         EmpWageBuilder emp = new EmpWageBuilder();
         emp.computeDailyWage();
     }
+
+    int PART_TIME = 4;
+    int FULL_TIME = 8;
+    int wagePerHour = 20;
+
+    int empType = (int)(Math.random() * 3);
+
+    int hours = 0;
+
+    switch(empType) {
+        case 1:
+            hours = FULL_TIME;
+            break;
+        case 2:
+            hours = PART_TIME;
+            break;
+        default:
+            hours = 0;
+    }
+
+    int wage = hours * wagePerHour;
+    System.out.println("Employee Wage: " + wage);
 }
