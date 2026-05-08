2b0f97fe5b5fb9185b554c5eb96e24a5d5cdd17f
Tanushree Borase <tb2599@srmist.edu.in>
2026-05-08 17:20:14 +0530

    UC5-UC12: Added multiple Address Books, duplicate prevention, search, view, count, sorting features, and refactored using Streams

--- Day 24 Practice Problem/AddressBookSystem.java
+++ Day 24 Practice Problem/AddressBookSystem.java
@@ -9,4 +9,39 @@ public class AddressBookSystem {
         bookMap.put(name, book);
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        Contact c = (Contact) obj;
+        return this.firstName.equals(c.firstName);
+    }
+
+    public void searchByCity(String city) {
+    contacts.stream()
+            .filter(c -> c.city.equals(city))
+            .forEach(System.out::println);
+    }
+
+    public void viewByCity(String city) {
+    contacts.stream()
+            .filter(c -> c.city.equals(city))
+            .forEach(System.out::println);
+    }
+
+  public long countByCity(String city) {
+    return contacts.stream()
+            .filter(c -> c.city.equals(city))
+            .count();
+    }
+    
+    public void sortByName() {
+    contacts.stream()
+            .sorted((c1, c2) -> c1.firstName.compareTo(c2.firstName))
+            .forEach(System.out::println);
+    }
+
+    public void sortByCity() {
+    contacts.stream()
+            .sorted((c1, c2) -> c1.city.compareTo(c2.city))
+            .forEach(System.out::println);
+    }
 }
