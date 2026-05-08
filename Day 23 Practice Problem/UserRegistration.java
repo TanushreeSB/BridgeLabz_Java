24556d11ab1f3eb9e715a07c4bf7618460e9ea4f
Tanushree Borase <tb2599@srmist.edu.in>
2026-05-08 16:58:00 +0530

    UC2: Added Last Name validation (Starts with capital, min 3 characters)

--- Day 23 Practice Problem/UserRegistration.java
+++ Day 23 Practice Problem/UserRegistration.java
@@ -7,5 +7,10 @@ public class UserRegistration {
         return Pattern.matches(regex, firstName);
     }
 
+     public boolean validateLastName(String lastName) {
+        String regex = "^[A-Z][a-z]{2,}$";
+        return Pattern.matches(regex, lastName);
+    }
+
     
 }
