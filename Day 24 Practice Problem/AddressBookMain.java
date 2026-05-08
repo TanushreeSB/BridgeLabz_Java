d73a66007f0529337b869af5716e2f5d16a142b1
Tanushree Borase <tb2599@srmist.edu.in>
2026-05-08 17:12:34 +0530

    Initial commit: Welcome to Address Book program setup

---
+++ Day 24 Practice Problem/AddressBookMain.java
@@ -0,0 +1,7 @@
+package BridgeLabz_Java;
+
+public class AddressBookMain {
+     public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
---
+++ Day 24 Practice Problem/Contact.java
@@ -0,0 +1,31 @@
+package BridgeLabz_Java.Day 24 Practice Problem;
+
+public class Contact {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phone;
+    String email;
+
+    public Contact(String firstName, String lastName, String address,
+                   String city, String state, String zip,
+                   String phone, String email) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " " + city;
+    }
+}
