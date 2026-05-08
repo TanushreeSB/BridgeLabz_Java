1ffc1e0172594286c20dabccdc34bc599cd8c39e
Tanushree Borase <tb2599@srmist.edu.in>
2026-05-08 17:14:05 +0530

    UC1: Added Contact class and ability to add contact in Address Book

--- Day 24 Practice Problem/AddressBookMain.java
+++ Day 24 Practice Problem/AddressBookMain.java
@@ -3,5 +3,15 @@ package BridgeLabz_Java;
 public class AddressBookMain {
      public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        AddressBook book = new AddressBook();
+
+        Contact c1 = new Contact("Tanu", "Sharma", "Chennai",
+                "Chennai", "TN", "600001",
+                "9999999999", "tanu@gmail.com");
+
+        book.addContact(c1);
+
+        System.out.println(c1);
     }
 }
